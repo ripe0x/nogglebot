4fd287e8c217cec9128030b7957022695d9e0fb7
ripe <unripeengine@gmail.com>
2022-11-01 11:15:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 
-const client = new Client({
+const client = new Discord.Client({ 
     intents: [
-        GatewayIntentBits.Guilds,
-        // GatewayIntentBits.Messages,
-    ]
-})
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.DirectMessages
+    ] 
+});
 client.once('ready', () => {
     console.log('Ready!');
 });
@@ -14,8 +15,17 @@ client.on('interactionCreate', interaction => {
     console.log(interaction);
 });
 
+const swearWords = ["darn", "shucks", "frak", "shite"]; // Make sure all of the words are lowercased only.
+
 client.on("messageCreate", message => {
     console.log(message);
+    if (swearWords.some(word => message.content.toLowerCase().includes(word.toLowerCase()))) { // Lowercase the message content for better matching
+        message.reply("Oh no you said a bad word!!!");
+        // Or just do message.delete();
+    }
 });
+
+
+
 
 client.login(process.env.DISCORD_TOKEN);
